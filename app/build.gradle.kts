a3d905054fd6e64d9cc6e449bb7e7c6e6312f0d5
Louiza Ager <louizaager7@gmail.com>
2025-06-25 04:14:07 +0300

    alarm sound

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.alarmapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.alarmapp"
--- app/src/main/java/com/example/alarmapp/MathChallengeActivity.kt
+++ app/src/main/java/com/example/alarmapp/MathChallengeActivity.kt
@@ -3,6 +3,7 @@ package com.example.alarmapp
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -27,7 +28,32 @@ class MathChallengeActivity : AppCompatActivity() {
         val answer = findViewById<TextView>(R.id.answer)
         val submit = findViewById<TextView>(R.id.submit)
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm_sound)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
 
+        val num1 = (1..20).random()
+        val num2 = (1..20).random()
+        correctAnswer = (num1 + num2).toString()
+        mathQuestion.text = "What is $num1 + $num2 ?"
+
+        submit.setOnClickListener(
+            {
+                if (answer.text.toString() == correctAnswer) {
+                    mediaPlayer.stop()
+                    Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
 
     }
 }
